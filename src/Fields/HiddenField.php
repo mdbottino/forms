398ff3a8fd6e400e5003db3bd9bf2d6bb8113ca8
Marcelo Daniel Bottino <mdbottino@gmail.com>
2019-06-20 20:16:31 -0300

    Simplifies HiddenField

--- src/Fields/HiddenField.php
+++ src/Fields/HiddenField.php
@@ -2,21 +2,13 @@
 
 namespace mdbottino\Forms\Fields;
 
-use mdbottino\Forms\BaseField;
+use mdbottino\Forms\BaseInputField;
 
-class HiddenField extends BaseField {
+class HiddenField extends BaseInputField {
 
     protected $type = 'hidden';
 
     public function label(){
         return "";
-    }
-
-    public function widget($old){
-        $type = $this->type();
-        $id = $this->id();
-        $name = $this->name();
-        $data = !is_null($old) ? $old : $this->data;
-        return "<input id='$id' name='$name' type='$type' value='$data'> \n";
     }
 }
