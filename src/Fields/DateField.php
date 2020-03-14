da8606df48e847ec4debaadac10da57a5b124d52
Marcelo Daniel Bottino <mdbottino@gmail.com>
2020-03-14 15:15:44 -0300

    Change: Reformat data to match default date format

--- src/Fields/DateField.php
+++ src/Fields/DateField.php
@@ -6,5 +6,18 @@ use mdbottino\Forms\BaseInputField;
 
 class DateField extends BaseInputField {
 
+    const DATE_FORMAT = 'Y-m-d';
     protected $type = 'date';
+
+    public function data($old = null){
+        $value = !is_null($old) ? $old : $this->data;
+
+        $timestamp = strtotime($value);
+
+        if ($timestamp === false){
+            return '';
+        } else {
+            return date($this::DATE_FORMAT , $timestamp);
+        }
+    }
 }
