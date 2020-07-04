557f3816531da8afb677f10d1af965ac8429a50b
Marcelo Daniel Bottino <mdbottino@gmail.com>
2020-07-04 20:51:23 -0300

    Change: Updates the field to the new format

--- src/Fields/CheckboxField.php
+++ src/Fields/CheckboxField.php
@@ -7,15 +7,12 @@ use mdbottino\Forms\BaseInputField;
 class CheckboxField extends BaseInputField {
 
     protected $valid_field_attrs = ['checked'];
-    protected $template = "<input id=':id' name=':name' type=':type' :checked :attrs :required>";
     protected $type = 'checkbox';
 
     protected function vars($old=null){
         $data = $this->data($old);
+        $this->attrs['checked'] = (boolean) $data ? 'checked' : '';
 
-        $vars = parent::vars($old);
-        $vars[':checked'] = (boolean) $data ? 'checked' : '';
-
-        return $vars;
+        return parent::vars($old);
     }
 }
